d845529e739f9e4aa1a0e54270bde58780181124
codewithmaik <mcawelius@web.de>
2023-05-22 17:08:39 +0000

    Store image-container and loader in variables and update data variable to photos array

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,8 @@
+const imageContainer = document.getElementById("image-container");
+const loader = document.getElementById("loader");
+
+let photosArray = [];
+
 // Unsplash API 
 const count = 10;
 const apiKey = "pSVNEmCLtniBwgXD8RNf9Ipilb_B-r6RBbpmZdwSu1c";
@@ -7,8 +12,8 @@ const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&coun
 async function getRandomPhotos() {
     try {
         const response = await fetch(apiUrl);
-        const data = await response.json();
-        console.log(data);
+        photosArray = await response.json();
+        console.log(photosArray);
     } catch (error) {
         // Catch error here
     }
