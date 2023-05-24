c9a56721a38a0f8eac63d11ae0ba2c17c9e09012
codewithmaik <mcawelius@web.de>
2023-05-24 15:18:34 +0000

    Adjust comments in displayPhotos() function

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,18 +15,22 @@ function setAttributes(element, attributes) {
     }
 }
 
-// Create elements for links and photos and add to DOM
+// Create elements for links and photos
 function displayPhotos() {
     photosArray.forEach((photo) => {
-        // Create <a> element that links to Unsplash
+        // Create <a> element and set attributes
         const item = document.createElement("a");
-        item.setAttribute("href", photo.links.html);
-        item.setAttribute("target", "_blank");
-        // Create <img> element for photo
+        setAttributes(item, {
+            href: photo.links.html,
+            target: "_blank"
+        })
+        // Create <img> element for photo and set attributes
         const image = document.createElement("img");
-        image.setAttribute("src", photo.urls.regular);
-        image.setAttribute("alt", photo.alt_description);
-        image.setAttribute("title", photo.alt_description);
+        setAttributes(image, {
+            src: photo.urls.regular,
+            alt: photo.alt_description,
+            title: photo.alt_description
+        })
         // Put <img> element in <a> element and put them together into the imageContainer
         item.appendChild(image);
         imageContainer.appendChild(item);
